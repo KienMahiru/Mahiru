e06a864155e3619f771ca213d8fe4ef6a0759a6c
admin <hoangvinhsctn@gmail.com>
2024-01-01 17:18:49 +0700

    Toi uu phan splah.java

--- app/src/main/java/com/example/doan/activity/splah.java
+++ app/src/main/java/com/example/doan/activity/splah.java
@@ -1,12 +1,9 @@
 package com.example.doan.activity;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.example.doan.R;
@@ -14,33 +11,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class splah extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 3000;
     ImageView i1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splah);
-        i1=(ImageView)findViewById(R.id.i1);
+        i1 = (ImageView) findViewById(R.id.i1);
         Glide.with(this)
                 .load(R.drawable.logo)
                 .transform(new CircleCrop())
                 .into(i1);
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                nextActivity();
-            }
-        },3000);
 
+        new Handler().postDelayed(this::nextActivity, SPLASH_DELAY);
     }
 
     private void nextActivity() {
-        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
-        if(user==null) {//chua login
-            Intent intent1=new Intent(splah.this,Dangnhap.class);
-            startActivity(intent1);
-        }else {
-            Intent intent=new Intent(splah.this,FullscreenImageActivity.class);
-            startActivity(intent);
-        }
-    }}
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        startActivity(new Intent(splah.this, user == null ? Dangnhap.class : FullscreenImageActivity.class));
+    }
+}
