678260ca1eb85eddded37fdad7572a39a6d9a7d4
admin <hoangvinhsctn@gmail.com>
2024-01-02 12:33:34 +0700

    Toi uu code phan feedback

--- app/src/main/java/com/example/doan/fragment/FeedbackFragment.java
+++ app/src/main/java/com/example/doan/fragment/FeedbackFragment.java
@@ -2,7 +2,6 @@ package com.example.doan.fragment;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
@@ -11,54 +10,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.widget.Button;
-import android.widget.EditText;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.doan.R;
 
-
 public class FeedbackFragment extends Fragment {
     private View mView;
-    private EditText feedback;
     private Button sendButton,sendphone;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_feed, container, false);
         requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        // Lấy đối tượng ActionBar
-        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
-        // Thiết lập tiêu đề mới cho ActionBar
-        if (actionBar != null) {
-            actionBar.setTitle("Phản hồi ");
-        }
 
-        sendButton = mView.findViewById(R.id.send_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendEmail();
-            }
-        });
-        sendphone = mView.findViewById(R.id.sendphone);
-        sendphone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String phoneNumber = "012345678"; // Thay số điện thoại tại đây
-                dialPhoneNumber(phoneNumber);
-            }
-        });
-
-
+        setupActionBar("Phản hồi");
+        setupViews();
         return mView;
     }
-    private  void  sendEmail(){
 
+    public void setupActionBar(String text) {
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(text);
+        }
+    }
+
+    private void setupViews() {
+        sendButton = mView.findViewById(R.id.send_button);
+        sendphone = mView.findViewById(R.id.sendphone);
+
+        sendButton.setOnClickListener(v -> sendEmail());
+        sendphone.setOnClickListener(v -> dialPhoneNumber("012345678"));
+    }
+
+    private void sendEmail(){
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                 "mailto", "meomeo123@gmail.com", null));
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
-
         startActivity(Intent.createChooser(emailIntent, "Send email..."));
     }
     private void dialPhoneNumber(String phoneNumber) {
